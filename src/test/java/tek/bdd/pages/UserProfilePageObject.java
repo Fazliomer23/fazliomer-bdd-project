96019b2cd2cafec8d43b7c617f7185e98e1bee42
fazliomerali <fazliomerkhan@gmail.com>
2024-09-02 17:39:09 -0400

    fazli omer final bdd project

--- src/test/java/tek/bdd/pages/UserProfilePageObject.java
+++ src/test/java/tek/bdd/pages/UserProfilePageObject.java
@@ -5,6 +5,7 @@ import tek.bdd.utilities.SeleniumUtility;
 
 public class UserProfilePageObject extends SeleniumUtility {
     public static final By ACCOUNT_PROFILE = By.xpath("//button[@type='button']");
+    public static final By PROFILE_INFORMATION = By.xpath("//header[text()='Profile']");
 
 
     public static final By LOGOUT_BUTTON = By.xpath("(//button[@type='button'])[3]");

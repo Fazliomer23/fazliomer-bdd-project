3d4162a8b5d82a2f660e86f19fab83b1fe38df66
fazliomerali <fazliomerkhan@gmail.com>
2024-09-02 17:32:36 -0400

    fazli omer final bdd project

--- src/test/java/tek/bdd/browsers/EdgeBrowser.java
+++ src/test/java/tek/bdd/browsers/EdgeBrowser.java
@@ -8,6 +8,7 @@ public class EdgeBrowser implements BaseBrowser {
 
     @Override
     public WebDriver openBrowser(boolean isHeadless){
+
         EdgeOptions options = new EdgeOptions();
         if (isHeadless) options.addArguments("--headless");
         return new EdgeDriver(options);
